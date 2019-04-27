66f7ce285af680b52e701d2e9b3cd28081855aa8
Allen-Sarmiento <allensarmiento3@gmail.com>
2019-04-27 00:28:31 -0700

    Pseudocode for functions

--- predict-seasons.cu
+++ predict-seasons.cu
@@ -3,8 +3,66 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Takes in a percentage of the weather data as training data at random.
+void trainData(float percentage, Weather data) {
+}
+
+// Evaluates which data is not used in the training data and sets those
+// as testing data.
+void testData(Weather train, Weather data) {
+}
+
+// Use the training data to predict the testing data.
+// Compare the predictions to the actual values.
+void predict(Weather train, Weather test) {
+    // This is where kMeans is called
+    // Call function kNN(points, centroids, test) 
+    //    - This function uses the kMeans to predict the test data
+}
+
+// Generate random centroid locations
+void generateCentroids() {
+}
+
 // Perform k-means clustering 
 void kMeans() {
+  /* Approach:
+      1. Identify the closest points to each centroid; this forms the
+         grouping.
+      2. In parallel, calculate the euclidean distance between each centroid
+         and points in its grouping.
+      3. If the euclidean distance is not proportional, move centroids and
+         repeat calculation
+      4. Centroids are now in place, can now apply training and testing data
+  */
+
+  /* Pseudocode:
+     
+     # NOTE: This can be done in parallel, partition the points
+     #       and pass in the coordinates of each centroid to evaluate
+     for point in points:
+        min_distance = None 
+        centroid_number = None
+        for centroid in centroids:
+          if euclideanDistance(point, centroid) < min_distance:
+              min_distance = euclideanDistance(point, centroid)
+              centroid_number = centroid
+        addGrouping(point, centroid_number)
+
+     # In parallel, calculate the distance between each point and its
+     # centroid.
+     values = []
+     for points in centroid_group:
+        values.append( euclideanDistance(points, centroid) ) 
+            
+     if centroid location is bad:
+        # Call function recursively
+        kMeans(points, centroids)
+  */
+}
+
+// Takes in the test data, points, and centroids to make predictions.
+void kNN() {
 }
 
 // Calculate the euclidean distance between two points.

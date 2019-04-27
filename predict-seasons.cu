d8e0f7dcf808acd50424a1666e7f3018934fc0d2
Allen-Sarmiento <allensarmiento3@gmail.com>
2019-04-26 23:14:22 -0700

    Starting implementation

---
+++ predict-seasons.cu
@@ -0,0 +1,19 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Perform k-means clustering 
+void kMeans() {
+}
+
+// Calculate the euclidean distance between two points.
+double euclideanDistance(int x1, int y1, int x2, int y2) {
+  // NOTE: Pow might need to take in a double, check if this works.
+  return sqrt( pow(x1 - x2, 2) + pow(y1 - y2, 2) )
+}
+
+int main() {
+
+  return 0;
+}

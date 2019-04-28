75dfc6d9bf955e14dab7f3f1d6b9bea4172636c7
Ryan <ryan.shim@gmail.com>
2019-04-28 05:57:32 -0700

    Finished data processing and dataset generator function

--- predict-seasons.c
+++ predict-seasons.c
@@ -3,24 +3,45 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Reads in the specified column values from the csv filename.
-int * generateData(char fname[], int col) {
+#define ARRAYSIZE 3321
+
+typedef struct Weather {
+  int month;
+  float temp;
+  float slp;
+} Weather;
+
+// Takes in a csv file specified by fname and returns a pointer to an array of
+// weather objects.
+Weather* generateData(char fname[]) {
   char* token;
   char line[30];
   const char delim[2] = ",";
+  int i = 0;
+
+  // allocate mem to hold weather data
+  Weather* weatherData = (Weather *)malloc(sizeof(Weather)*ARRAYSIZE);
 
   FILE* instream = fopen(fname, "r");
 
+  // Retrieve each line until eof. Each line should contain
+  // no more than 30 characters.
   while (fgets(line, 30, instream)) {
-    token = strtok(line, delim); // get the first token
-
-    while (token != NULL) {
-      printf("%s\n", token);
-      token = strtok(NULL, delim);
-    }
+    Weather w;
+    token = strtok(line, delim);
+    w.month = atoi(token);
+    token = strtok(NULL, delim);
+    w.temp = atof(token);
+    token = strtok(NULL, delim);
+    w.slp = atof(token);
+    weatherData[i] = w;
+    i++;
   }
+  fclose(instream);
+  return weatherData;
 }
 
+/*
 // Takes in a percentage of the weather data as training data at random.
 void trainData(float percentage, Weather data) {
 }
@@ -46,41 +67,39 @@ void generateCentroids() {
 
 // Perform k-means clustering.
 // Centroids and points should be passed in.
-void kMeans() {
-  /* Approach:
-      1. Identify the closest points to each centroid; this forms the
-         grouping.
-      2. In parallel, calculate the euclidean distance between each centroid
-         and points in its grouping.
-      3. If the euclidean distance is not proportional, move centroids and
-         repeat calculation
-      4. Centroids are now in place, can now apply training and testing data
-  */
-
-  /* Pseudocode:
-     
-     # NOTE: This can be done in parallel, partition the points
-     #       and pass in the coordinates of each centroid to evaluate
-     for point in points:
-        min_distance = None 
-        centroid_number = None
-        for centroid in centroids:
-          if euclideanDistance(point, centroid) < min_distance:
-              min_distance = euclideanDistance(point, centroid)
-              centroid_number = centroid
-        addGrouping(point, centroid_number)
-
-     # In parallel, calculate the distance between each point and its
-     # centroid.
-     values = []
-     for points in centroid_group:
-        values.append( euclideanDistance(points, centroid) ) 
-            
-     if centroid location is bad:
-        # Call function recursively
-        kMeans(points, centroids)
-  */
-}
+//void kMeans() {
+  // Approach:
+  //    1. Identify the closest points to each centroid; this forms the
+  //       grouping.
+  //    2. In parallel, calculate the euclidean distance between each centroid
+  //       and points in its grouping.
+  //    3. If the euclidean distance is not proportional, move centroids and
+  //       repeat calculation
+  //    4. Centroids are now in place, can now apply training and testing data
+  //
+  // Pseudocode:
+  //   
+  //   # NOTE: This can be done in parallel, partition the points
+  //   #       and pass in the coordinates of each centroid to evaluate
+  //   for point in points:
+  //      min_distance = None 
+  //      centroid_number = None
+  //      for centroid in centroids:
+  //        if euclideanDistance(point, centroid) < min_distance:
+  //            min_distance = euclideanDistance(point, centroid)
+  //            centroid_number = centroid
+  //      addGrouping(point, centroid_number)
+  //
+  //   # In parallel, calculate the distance between each point and its
+  //   # centroid.
+  //   values = []
+  //   for points in centroid_group:
+  //      values.append( euclideanDistance(points, centroid) ) 
+  //          
+  //   if centroid location is bad:
+  //      # Call function recursively
+  //      kMeans(points, centroids)
+//}
 
 // Takes in the test data, points, and centroids to make predictions.
 void kNN() {
@@ -91,9 +110,12 @@ double euclideanDistance(int x1, int y1, int x2, int y2) {
   // NOTE: Pow might need to take in a double, check if this works.
   return sqrt( pow(x1 - x2, 2) + pow(y1 - y2, 2) );
 }
+*/
 
 int main() {
-  char file1[] = "./data/socal_weather_2010.csv";
-  generateData(file1, 1);
+  char filename[] = "./data/socal_weather.csv";
+  printf("Generating dataset\n");
+  Weather* data = generateData(filename);
+  printf("Dataset created.\n");
   return 0;
 }
